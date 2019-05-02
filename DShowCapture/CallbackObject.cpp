e14755f49913d12050493b24d1257cecc6f0e84f
siryang2006 <393121997@qq.com>
2019-05-03 07:02:18 +0800

    Update CallbackObject.cpp

--- DShowCapture/CallbackObject.cpp
+++ DShowCapture/CallbackObject.cpp
@@ -9,14 +9,14 @@ CallbackObject::CallbackObject()
 
 STDMETHODIMP CallbackObject::SampleCB(double SampleTime, IMediaSample *pSample)
 {
-    /*if (!pSample)
+    if (!pSample)
         return E_POINTER;
     long sz = pSample->GetActualDataLength();
     BYTE *pBuf = NULL;
     pSample->GetPointer(&pBuf);
     if (sz <= 0 || pBuf == NULL) return E_UNEXPECTED;
     for (int i = 0; i < sz; i += 2)
-        pBuf[i] = 255 - pBuf[i];*/
+        pBuf[i] = 255 - pBuf[i];
     pSample->Release();
     return S_OK;
 }
